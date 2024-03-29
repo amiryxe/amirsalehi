8948665f5babd53716e98affc466ca36a488f0a2
Amir Salehi <nik.amirsa@gmail.com>
2024-03-30 02:07:04 +0330

    create some components

---
+++ src/components/layout.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const Layout = ({ pageTitle, children }: { pageTitle: string; children: React.ReactNode }) => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>I'm making this by following the about Tutorial.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
