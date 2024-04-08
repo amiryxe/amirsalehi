ed5035df1d78aa062a364198fbac1a36b3b07491
Amir Salehi <nik.amirsa@gmail.com>
2024-04-09 00:16:37 +0330

    render images from mdx to blog post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = ({ data, children }: any) => {
+  console.log(data)
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image!} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{' '}
+        <a href={data.mdx.frontmatter.hero_image_credit_link} target="_blank">
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+
       {children}
     </Layout>
   )
@@ -17,7 +30,15 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
