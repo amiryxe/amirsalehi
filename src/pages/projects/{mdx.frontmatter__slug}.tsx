4b3565ed5c9d11952002af89cabf1b9ac0cf4738
Amir Salehi <nik.amirsa@gmail.com>
2024-07-17 01:27:53 +0330

    some example projects

---
+++ src/pages/projects/{mdx.frontmatter__slug}.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+
+import { graphql, Link } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+
+const ProjectPage = ({ data, children, pageContext }: any) => {
+  console.log(data)
+
+  return (
+    <Layout>
+      <h1 className="text-2xl font-extrabold">{data.mdx.frontmatter.title}</h1>
+
+      <div className="mb-16 post">{children}</div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+
+export const Head = ({ data }: any) => <Seo title={data.mdx.frontmatter.title} />
+
+export default ProjectPage
